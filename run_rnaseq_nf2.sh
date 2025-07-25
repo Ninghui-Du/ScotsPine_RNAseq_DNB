08e9c590bb108ceba579a06409e932f01672b487
Ninghui-Du <nindu@ceh.ac.uk>
2025-07-25 13:57:24 +0100

    Update run_rnaseq_nf2.sh

--- run_rnaseq_nf2.sh
+++ run_rnaseq_nf2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --output=/mnt/shared/scratch/ndu/pine_project/output/pinebiome_full.log
+#SBATCH --output= input-dir/pinebiome_full.log
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=4G
 #SBATCH --partition=long
@@ -16,12 +16,12 @@ export NXF_SINGULARITY_PULL_TIMEOUT=72h
 nextflow run nf-core/rnaseq -profile cropdiversityhpc \
                                	-resume \
 				-process.shell bash \
-				-c '/mnt/shared/scratch/ndu/pine_project/scripts/slurm2.config' \
+				-c 'input-dir/slurm2.config' \
 				-r dev \
-				--input '/mnt/shared/scratch/ndu/pine_project/data/samplesheet_full.csv' \
-				--outdir '/mnt/shared/scratch/ndu/pine_project/output/full_output/' \
-				--fasta '/mnt/shared/scratch/ndu/pine_project/genome/ptabuliformis.fa' \
-				--gtf '/mnt/shared/scratch/ndu/pine_project/genome/P.tabuliformis_V1.0.gene.final.renamed.gtf' \
+				--input 'input-dir/samplesheet_full.csv' \
+				--outdir 'output-dir/full_output/' \
+				--fasta 'input-dir/ptabuliformis.fa' \
+				--gtf 'input-dir/P.tabuliformis_V1.0.gene.final.renamed.gtf' \
 				--aligner 'star_salmon' \
 				--remove_ribo_rna \
 				--save_reference \
